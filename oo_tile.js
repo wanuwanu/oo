02170a97fcc0cdc862f93b66ce5a5bd1f918cc0b
wanuwanu <wanuwanu@gmail.com>
2018-04-17 08:02:27 +0900

    oo

--- oo_tile.js
+++ oo_tile.js
@@ -1,11 +1,11 @@
 // OoLibrary Copyright (c) wanu@nyagoya
 // Released under the MIT license http://opensource.org/licenses/mit-license.php
 
-class OoSprite extends OoDrawObject {
+class OoTile extends OoDrawObject {
   constructor() {
     super();
     this.size = new Oo2DVector(1);
-    this.image = null;
+    this.color = '#ffffff';
   }
 
   draw(context) {
@@ -16,24 +16,23 @@ class OoSprite extends OoDrawObject {
     oo.localAlpha(ctx, this.alpha, () => {
       const sx = this.size.x * this.scale.x;
       const sy = this.size.y * this.scale.y;
+      ctx.fillStyle = this.color;
       if (this.rotation.z === 0) {
-        ctx.drawImage(this.image, this.position.x - sx * 0.5, this.position.y - sy * 0.5, sx, sy);
+        ctx.fillRect(this.position.x - sx * 0.5, this.position.y - sy * 0.5, sx, sy);
       } else {
         ctx.save();
         ctx.translate(this.position.x, this.position.y);
         ctx.rotate(this.rotation.z);
-        ctx.drawImage(this.image, - sx * 0.5, - sy * 0.5, sx, sy);
+        ctx.fillRect(- sx * 0.5, - sy * 0.5, sx, sy);
         ctx.restore();
       }
     });
   }
 
-  static create(image) {
-    const o = new OoSprite();
-    if (image instanceof Image) {
-      o.image = image;
-      o.size.set(image.width, image.height);
-    }
+  static create(color, width, height) {
+    const o = new OoTile();
+    o.color = color;
+    o.size.set(width, height);
     return o;
   }
 }
